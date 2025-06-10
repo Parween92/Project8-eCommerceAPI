090b11cc2b6ffdc2d6a2e22461c6e1abf395640b
Julia <kret.julia@gmail.com>
2025-06-10 17:45:50 +0200

    users created done

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import "./db/associations.js";
 import express from "express";
-import postRouter from "./routers/postRouter";
 import userRouter from "./routers/userRouter.js";
-import errorHandler from "./middlewares/errorHandler.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.use("/users", postRouter);
-app.use("/category", userRouter);
-app.use("/products", userRouter);
-app.use("/orders", userRouter);
+app.use("/users", userRouter);
+// app.use("/category", categoryRouter);
+// app.use("/products", productRouter);
+// app.use("/orders", orderRouter);
 
 app.use(errorHandler);
 
